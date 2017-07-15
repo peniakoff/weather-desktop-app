0df3afabd98d0d2fc83379c6c9f58efd84fd2d05
Tomasz Miller <info@tomaszmiller.pl>
2017-07-15 18:05:57 +0200

    observer patter for weather update

--- src/main/java/pl/tomaszmiller/models/services/WeatherInfo.java
+++ src/main/java/pl/tomaszmiller/models/services/WeatherInfo.java
@@ -4,4 +4,48 @@ package pl.tomaszmiller.models.services;
  * Created by Peniakoff on 15.07.2017.
  */
 public class WeatherInfo {
+    private double temp;
+    private int pressure;
+    private int humidity;
+    private int cloudAll;
+    private String cityName;
+
+    public WeatherInfo(double temp, int pressure, int humidity, int cloudAll, String cityName) {
+        this.temp = temp;
+        this.pressure = pressure;
+        this.humidity = humidity;
+        this.cloudAll = cloudAll;
+        this.cityName = cityName;
+    }
+
+    public WeatherInfo() {
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public double getCelsius() {
+        return temp - 273.15;
+    }
+
+    public double getFahrenheit() {
+        return temp * 9/5 - 459.67;
+    }
+
+    public int getPressure() {
+        return pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getCloudAll() {
+        return cloudAll;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
 }
--- src/main/java/pl/tomaszmiller/models/services/WeatherObserver.java
+++ src/main/java/pl/tomaszmiller/models/services/WeatherObserver.java
@@ -4,4 +4,7 @@ package pl.tomaszmiller.models.services;
  * Created by Peniakoff on 15.07.2017.
  */
 public interface WeatherObserver {
+
+    void updateWeather(WeatherInfo weatherInfo);
+
 }
