1a6088f1de2405c505cc409c6c645b87d133567f
Tomasz Miller <info@tomaszmiller.pl>
2017-07-15 17:18:23 +0200

    new method for connecting and responding

--- src/main/java/pl/tomaszmiller/models/Utils.java
+++ src/main/java/pl/tomaszmiller/models/Utils.java
@@ -1,7 +1,30 @@
-package pl.tomaszmiller.models.services;
+package pl.tomaszmiller.models;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * Created by Peniakoff on 15.07.2017.
  */
 public class Utils {
+
+    public static String connectAndResponse(String url) {
+        StringBuffer stringBuffer = new StringBuffer(); //thread's safe!
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
+    }
+
 }
