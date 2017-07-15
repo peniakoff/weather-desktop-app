dd2f96a9dbca0bcddbf43e44fac19c3d6da208ce
Tomasz Miller <info@tomaszmiller.pl>
2017-07-15 17:17:34 +0200

    resource renamed

--- src/main/java/pl/tomaszmiller/Main.java
+++ src/main/java/pl/tomaszmiller/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainView.fxml"));
         primaryStage.setTitle("Weather Desktop App");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.setResizable(false);
